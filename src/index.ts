4a8f134e427bcb2a374835f0848c70a7fd959ee9
artem <mstrJesm@gmail.com>
2021-11-21 16:26:39 +0200

    add customMap class, add google maps types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import { Company } from './components/Company';
-import { User } from './components/User';
+// import { Company } from './components/Company';
+// import { User } from './components/User';
+import { CustomMap } from './components/CustomMap';
 
-const user = new User();
-const company = new Company();
-console.log(user, company);
+new CustomMap('map');
